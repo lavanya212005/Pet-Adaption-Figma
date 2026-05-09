7f44de8dcf8279236fd3cc39d929bba7ef7f5cfb
lavaniviji2005@gmail.com <lavaniviji2005@gmail.com>
2026-05-09 17:47:13 +0530

    second

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
+  basePath: '/Pet-Adaption-Figma',
+  assetPrefix: '/Pet-Adaption-Figma/',
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
